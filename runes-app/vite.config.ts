e3b54f39ba2352788ad81350c7ce203d15a4a688
Eric Laugier <eric.laugier@egen.guru>
2022-12-03 12:47:17 +0100

    change config

--- runes-app/vite.config.ts
+++ runes-app/vite.config.ts
@@ -2,10 +2,21 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    base: "runes/"
-  },
-  base: "./",
-  plugins: [react()]
+export default defineConfig(({ command, mode, ssrBuild }) => {
+  if (command === 'serve') {
+    return {
+      // dev specific config
+      plugins: [react()]
+    }
+  } else {
+    // command === 'build'
+    return {
+      server: {
+        base: "runes/"
+      },
+      base: "runes/",
+      plugins: [react()]
+    }
+  }
 })
+
